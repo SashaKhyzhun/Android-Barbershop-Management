564bfe432742321f29aad517ad9e3ad348db390c
SashaKhyzhun <sasha.khyzhun@gmail.com>
2018-09-12 10:26:40 +0300

    methods for calendar and time.

--- app/src/main/java/com/sashakhyzhun/androidbarbershopmanagementprototype/ui/monthly/MonthlyActivity.kt
+++ app/src/main/java/com/sashakhyzhun/androidbarbershopmanagementprototype/ui/monthly/MonthlyActivity.kt
@@ -1,12 +1,10 @@
 package com.sashakhyzhun.androidbarbershopmanagementprototype.ui.monthly
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.marcohc.robotocalendar.RobotoCalendarView
 import com.sashakhyzhun.androidbarbershopmanagementprototype.R
-import java.text.SimpleDateFormat
 import java.util.*
 import android.widget.Toast
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
@@ -16,24 +14,71 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 class MonthlyActivity : AppCompatActivity(), RobotoCalendarView.RobotoCalendarListener {
 
+    companion object {
+        const val oneDay: Int = 24 * 60 * 60 * 1000
+    }
+
     private lateinit var calendarView: RobotoCalendarView
 
-    @SuppressLint("SimpleDateFormat")
-    private val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-    private val date = Date()
+    private val calendar = Calendar.getInstance()
+    private val now = System.currentTimeMillis()
+    private val today = Date(now)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monthly)
 
-        val millis = System.currentTimeMillis()
-        val date = Date(millis)
-
         calendarView = findViewById(R.id.calendarView)
-        calendarView.setDate(date)
+        calendarView.setDate(today)
         calendarView.setRobotoCalendarListener(this)
 
+        loadCurrentMonth()
+
     }
+
+    private fun loadCurrentMonth() {
+        val year = getCurrentYear()
+        println("year: $year")
+        val month = getCurrentMonth()
+        println("month: $month")
+        val day = getCurrentDay()
+        println("day: $day")
+
+
+        val daysInMonth = getDaysInMonth(year, month)
+        println("daysInMonth: $daysInMonth")
+
+        val firstDayMillis = getFirstDayOfMonthInMillis(year, month)
+
+        calendarView.markCircleImage1(Date(firstDayMillis))
+
+
+        for (i in 0 until daysInMonth) {
+            print("i=$i")
+//            if (d % 10 == 0) { continue } // skip every 10, 20, 30 day
+//            calendarView.markCircleImage1(Date(firstDayMillis + oneDay))
+        }
+
+
+    }
+
+    private fun getCurrentYear(): Int = calendar.get(Calendar.YEAR)
+    private fun getCurrentMonth(): Int = calendar.get(Calendar.MONTH) + 1
+    private fun getCurrentDay(): Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+    private fun getDaysInMonth(year: Int, month: Int): Int {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month - 1, 12)
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+    }
+
+    private fun getFirstDayOfMonthInMillis(year: Int, month: Int): Long {
+        val cal = GregorianCalendar(year, month - 1, 1)
+        cal.timeZone = TimeZone.getDefault()
+        return cal.time.time
+    }
+
+
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
