b059e0c303f1151bf797b53f269aabbeddeb8c8e
SashaKhyzhun <sasha.khyzhun@gmail.com>
2018-09-13 16:29:11 +0300

    dialog, confirmation, daily view done

--- app/src/main/java/com/sashakhyzhun/androidbarbershopmanagementprototype/ui/daily/DailyActivity.kt
+++ app/src/main/java/com/sashakhyzhun/androidbarbershopmanagementprototype/ui/daily/DailyActivity.kt
@@ -1,5 +1,6 @@
 package com.sashakhyzhun.androidbarbershopmanagementprototype.ui.daily
 
+import android.content.Context
 import android.graphics.RectF
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -9,6 +10,7 @@ import com.alamkanak.weekview.WeekView
 import com.alamkanak.weekview.WeekViewEvent
 import com.sashakhyzhun.androidbarbershopmanagementprototype.R
 import com.sashakhyzhun.androidbarbershopmanagementprototype.ui.common.BarberExtras
+import com.sashakhyzhun.androidbarbershopmanagementprototype.utils.CustomUI
 import java.util.*
 
 open class DailyActivity : AppCompatActivity(), BarberExtras,
@@ -17,14 +19,19 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
     private lateinit var extraDate: Date
     private lateinit var calendar: Calendar
     private lateinit var mWeekView: WeekView
+    private lateinit var ctx: Context
     private var extraIsFreeDay: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily)
+        ctx = this
 
         extraDate = intent?.extras?.get(dateKey) as Date
         extraIsFreeDay = intent?.extras?.get(freeDayKey) as Boolean
+
+
 
         println("is busy day: $extraIsFreeDay")
 
@@ -36,15 +43,17 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
         mWeekView = findViewById(R.id.weekView)
         // Set selected date
         mWeekView.goToDate(calendar)
+        // Make out calendar smaller
+        mWeekView.hourHeight = 1
         // Disabling horizontal scrolling
         mWeekView.xScrollingSpeed = 0.0F
         // Set an action when any event is clicked.
         mWeekView.setOnEventClickListener(this)
         // The week view has infinite scrolling horizontally. We have to provide the events of a
         // month every time the month changes on the week view.
-        mWeekView.monthChangeListener = MonthLoader.MonthChangeListener { year, month ->
+        mWeekView.monthChangeListener = MonthLoader.MonthChangeListener { _, _ ->
             // Populate the week view with some events.
-            getEvents()
+            if (extraIsFreeDay.not()) getEvents() else emptyList()
         }
 
         // Notify this shit about updates
@@ -54,6 +63,12 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
 
         mWeekView.emptyViewClickListener = WeekView.EmptyViewClickListener {
             Toast.makeText(this, "Tap: ${it.time}", Toast.LENGTH_SHORT).show()
+            val et = CustomUI.createEditText(ctx, null, R.string.edit_text_hint)
+            CustomUI.createAlertDialog(ctx, R.string.dialog_title, R.string.dialog_message,
+                    R.string.button_confirm, R.string.button_cancel, et) {
+
+                // do it
+            }
         }
 
         mWeekView.emptyViewLongPressListener = WeekView.EmptyViewLongPressListener {
@@ -79,7 +94,7 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
 
         start = Calendar.getInstance()
         start.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH), 11, 15, 0)
+                calendar.get(Calendar.DAY_OF_MONTH), 11, 5, 0)
 
         end = Calendar.getInstance()
         end.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
@@ -90,7 +105,7 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
 
         start = Calendar.getInstance()
         start.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH), 13, 15, 0)
+                calendar.get(Calendar.DAY_OF_MONTH), 13, 5, 0)
 
         end = Calendar.getInstance()
         end.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
@@ -100,7 +115,7 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
 
         start = Calendar.getInstance()
         start.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH), 15, 15, 0)
+                calendar.get(Calendar.DAY_OF_MONTH), 15, 5, 0)
 
         end = Calendar.getInstance()
         end.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
@@ -111,55 +126,6 @@ open class DailyActivity : AppCompatActivity(), BarberExtras,
 
         return weekViewList
     }
-
-/*
-    private fun getEvents(): MutableList<out WeekViewEvent> {
-        val weekViewList: MutableList<WeekViewEvent > = mutableListOf()
-
-
-        val calStart = Calendar.getInstance()
-        val calEnd = calStart.clone() as Calendar
-        calStart.time = extraDate
-
-
-         //9 - 11
-        calStart.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), 9, 0, 0)
-
-        calEnd.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), calStart.get(Calendar.HOUR_OF_DAY) + 2, 0, 0)
-
-        weekViewList.add(WeekViewEvent(1, "haircut session #1", calStart, calEnd))
-
-         //11 - 13
-        calStart.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), 11, 0, 0)
-
-        calEnd.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), calStart.get(Calendar.HOUR_OF_DAY) + 2, 0, 0)
-        weekViewList.add(WeekViewEvent(2, "haircut session #2", calStart, calEnd))
-
-         //13 - 15
-        calStart.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), 13, 0, 0)
-
-        calEnd.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), calStart.get(Calendar.HOUR_OF_DAY) + 2, 0, 0)
-        weekViewList.add(WeekViewEvent(3, "haircut session #3", calStart, calEnd))
-
-
-         //15 - 17
-        calStart.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), 15, 0, 0)
-
-        calEnd.set(calStart.get(Calendar.YEAR), calStart.get(Calendar.MONTH),
-                calStart.get(Calendar.DAY_OF_MONTH), calStart.get(Calendar.HOUR_OF_DAY) + 2, 0, 0)
-        weekViewList.add(WeekViewEvent(4, "haircut session #4", calStart, calEnd))
-
-
-        return weekViewList
-    }
-*/
 
 
     override fun onEventClick(event: WeekViewEvent?, eventRect: RectF?) {
